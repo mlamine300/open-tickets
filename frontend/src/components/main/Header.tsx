9923047bde9e1458a100eccabde3937983bc7165
lamine300 <laoufi.mohamed.lamine@gmail.com>
2026-03-03 12:13:54 +0100

    changing header

--- frontend/src/components/main/Header.tsx
+++ frontend/src/components/main/Header.tsx
@@ -2,10 +2,12 @@ import { useTheme } from "next-themes";
 import { HiOutlineSun, HiMoon } from "react-icons/hi2";
 const Header = () => {
   const { theme, setTheme } = useTheme();
-  const name=import.meta.env.VITE_APP_NAME||"Open Tickets";
+  const imageURlWhite=import.meta.env.VITE_HEADER_URL_WHITE||"";
+    const imageURlDark=import.meta.env.VITE_HEADER_URL_DARK||"";
+    const imageUrl=theme==="light"?imageURlWhite:imageURlDark;
   return (
-    <header className="fixed top-0 pt-2  w-full lg:max-w-screen  flex items-center z-10 border-b border-gray-hot px-10 py-2  bg-background-base">
-      <h4>{name} </h4>
+    <header className="fixed top-0 pt-2  w-full lg:max-w-screen  flex items-center z-10 border-b border-gray-hot px-10 py-2  bg-background-base max-h-16">
+      {imageUrl&&<img src={imageUrl} className="max-h-10 mx-2" /> }
       <button
         className="rounded-lg shadow-lg w-12 h-12 flex items-center justify-center ml-auto xl:mr-0"
         onClick={() => setTheme((t) => (t === "dark" ? "light" : "dark"))}
--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -6,17 +6,19 @@ const AuthLayout = ({ children }: { children: ReactElement }) => {
   const name=import.meta.env.VITE_APP_NAME ||"Open Tickets"; 
   return (
     <div className="flex flex-row w-full h-full ">
-      <div className="w-full  md:w-[60%] h-full flex flex-col justify-around p-10">
-        <h4 className="text-5xl my-8 italic text-amber-300">{name}</h4>
-        {children}
-      </div>
-      <div className="hidden md:w-[40%] md:flex items-center  justify-center">
+       <div className="hidden md:w-[60%] md:flex items-center  justify-center">
         <img
           src={image}
           alt="auth-side-img"
-          className="w-full h-full bg-cover object-cover"
+          className="w-full h-full bg-cover object-cover max-h-screen"
         />
       </div>
+      
+      <div className="w-full  md:w-[40%] h-full flex flex-col justify-around p-10">
+        <h4 className="text-5xl my-8 italic text-amber-300">{name}</h4>
+        {children}
+      </div>
+     
     </div>
   );
 };
